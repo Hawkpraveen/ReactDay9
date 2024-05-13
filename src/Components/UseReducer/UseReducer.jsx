bc87ecde36358a6ac893660fa93210e70bc7aa39
Praveen Kumar <hawkpraveen33@gmail.com>
2024-05-13 12:37:42 +0530

    updated

--- src/Components/UseReducer/UseReducer.jsx
+++ src/Components/UseReducer/UseReducer.jsx
@@ -6,13 +6,15 @@ const UseReducer = () => {
     return (
         <div className='container text-center  '>
             <h1>Using Reducer</h1>
-           
-            <button className='btn btn-success me-4' onClick={()=>{dispatch({type:'AddByTen',payload:10})}}>Click to add by 10</button>
+           <div className='mt-5'>
+           <button className='btn btn-success me-4' onClick={()=>{dispatch({type:'AddByTen',payload:10})}}>Click to add by 10</button>
             <button className='btn btn-danger ms-2' onClick={()=>{dispatch({type:'SubByTen',payload:10})}}>Click to minus by 10</button>
             <br /><br />
             <button className='btn btn-success me-4' onClick={()=>{dispatch({type:'Add'})}}>Click to add by 1</button>
             <button className='btn btn-danger ms-2' onClick={()=>{dispatch({type:'Sub'})}}>Click to minus by 1</button>
             <br />
+           </div>
+           
             <div className='container text-center mt-5 res'>
             {state}
             </div>
